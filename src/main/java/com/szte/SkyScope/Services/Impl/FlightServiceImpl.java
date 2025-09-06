23111974cfb815e06724538ea29d6b4a3be3cad1
Barát Marcell <barimarci1@gmail.com>
2025-09-06 15:59:23 +0200

    NEW: cache api token until expiry

--- src/main/java/com/szte/SkyScope/Services/Impl/FlightServiceImpl.java
+++ src/main/java/com/szte/SkyScope/Services/Impl/FlightServiceImpl.java
@@ -4,20 +4,25 @@ import com.szte.SkyScope.Config.ApplicationConfig;
 import com.szte.SkyScope.Models.AmadeusApiCred;
 import com.szte.SkyScope.Services.FlightService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class FlightServiceImpl implements FlightService {
 
-    private RestClient restClient = RestClient.create();
+    private final RestClient restClient = RestClient.create();
     private final ApplicationConfig applicationConfig;
 
     @Autowired
     public FlightServiceImpl(ApplicationConfig applicationConfig) {
         this.applicationConfig = applicationConfig;
     }
+
     @Override
+    @Cacheable("amadeusApiToken")
     public AmadeusApiCred getToken() {
         String body = "grant_type=client_credentials" +
                 "&client_id=" + applicationConfig.getAmadeusClientId() +
@@ -29,4 +34,8 @@ public class FlightServiceImpl implements FlightService {
                 .retrieve()
                 .body(AmadeusApiCred.class);
     }
+
+    @CacheEvict(value = "amadeusApiToken", allEntries = true)
+    @Scheduled(fixedRateString = "${amadeus_token_expiry}")
+    public void emptyAmadeusApiToken() {}
 }
