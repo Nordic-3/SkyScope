2355a9f980df537989402153160334ffcbbb6363
Barát Marcell <barimarci1@gmail.com>
2025-09-07 16:55:01 +0200

    NEW: advance search option with input validation

--- src/main/java/com/szte/SkyScope/Services/Impl/InputValidationServiceImpl.java
+++ src/main/java/com/szte/SkyScope/Services/Impl/InputValidationServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 
 @Service
 public class InputValidationServiceImpl implements InputValidationService {
@@ -15,10 +16,16 @@ public class InputValidationServiceImpl implements InputValidationService {
     private static final String DEPARTURE_BEFORE_TODAY = "Az indulás napja nem lehet korábbi a mai napnál!";
     private static final String RETURN_BEFORE_DEPARTURE = "A visszaút napja nem lehet korábbi az indulás napjánál!";
     private static final String INVALID_CITY_NAME = "Helyeten város név!";
+    private static final String INVALID_NUMBER = "Csak pozitív egész számot használjon!";
+    private static final String INVALID_TRAVEL_CLASS = "Kérjük a listából válasszon osztályt!";
 
     @Override
     public boolean isValidInputDatas(FlightSearch flightSearch, Model model) {
-        return !isEmptyInputFields(flightSearch, model) && isValidDate(flightSearch, model);
+        return !isEmptyInputFields(flightSearch, model)
+                && isValidDate(flightSearch, model)
+                && isNaturalNumber(flightSearch.getNumberOfAdults(), model)
+                && isValidTravelClass(flightSearch.getTravelClass(), model)
+                && isValidOptionalInputFileds(flightSearch, model);
     }
 
     @Override
@@ -47,6 +54,10 @@ public class InputValidationServiceImpl implements InputValidationService {
         }
         if (flightSearch.getDepartureDate() == null || flightSearch.getDepartureDate().isEmpty()) {
             model.addAttribute("errorOutDate", EMPTY_INPUT_ERROR);
+            isEmpty = true;
+        }
+        if (flightSearch.getNumberOfAdults() == null || flightSearch.getNumberOfAdults().isEmpty()) {
+            model.addAttribute("errorInAdvancedSearch", EMPTY_INPUT_ERROR);
             isEmpty = true;
         }
         if (!flightSearch.isOneWay()) {
@@ -82,5 +93,41 @@ public class InputValidationServiceImpl implements InputValidationService {
             }
         }
         return true;
+    }
+
+    private boolean isValidOptionalInputFileds(FlightSearch flightSearch, Model model) {
+        if (flightSearch.getNumberOfChildren() != null && !flightSearch.getNumberOfChildren().isEmpty()) {
+            return isNaturalNumber(flightSearch.getNumberOfChildren(), model);
+        }
+        if (flightSearch.getNumberOfInfants() != null && !flightSearch.getNumberOfInfants().isEmpty()) {
+            return isNaturalNumber(flightSearch.getNumberOfInfants(), model);
+        }
+        if (flightSearch.getMaxPrice() != null && !flightSearch.getMaxPrice().isEmpty()) {
+            return isNaturalNumber(flightSearch.getMaxPrice(), model);
+        }
+        return true;
+    }
+
+    private boolean isNaturalNumber(String numberToCheck, Model model) {
+        int number;
+        try {
+            number = Integer.parseInt(numberToCheck);
+        } catch (NumberFormatException exception) {
+            model.addAttribute("errorInAdvancedSearch", INVALID_NUMBER);
+            return false;
+        }
+        if (number <= 0) {
+            model.addAttribute("errorInAdvancedSearch", INVALID_NUMBER);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidTravelClass(String travelClass, Model model) {
+        if (Arrays.asList("ECONOMY", "PREMIUM_ECONOMY", "BUSINESS", "FIRST", "ALL").contains(travelClass)) {
+            return true;
+        }
+        model.addAttribute("errorInAdvancedSearch", INVALID_TRAVEL_CLASS);
+        return false;
     }
 }
