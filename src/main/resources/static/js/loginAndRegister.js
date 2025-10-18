62c880fda62f1506e6b2c8b871d412fb6d057627
Barát Marcell <barimarci1@gmail.com>
2025-10-18 11:52:26 +0200

    REF: open registration modal after invalid reg

--- src/main/resources/static/js/loginAndRegister.js
+++ src/main/resources/static/js/loginAndRegister.js
@@ -1,8 +1,8 @@
 function contentLoaded() {
-    if(window.location.href.includes("error")) {
-       document.getElementById("error").removeAttribute("hidden");
+    if (window.location.href.includes("error")) {
+        document.getElementById("error").removeAttribute("hidden");
     }
     if (window.location.href.includes("invalidPassword")) {
-        document.getElementById("reg").click();
+        new bootstrap.Modal(document.getElementById("registration")).show();
     }
 }
