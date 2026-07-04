8838205f74dde73e1e64c0789cbfd693cf847c45
Barát Marcell <barimarci1@gmail.com>
2026-07-04 15:05:35 +0200

    NEW: spring boot version upgrade

--- src/test/java/com/szte/skyScope/RunCucumberTest.java
+++ src/test/java/com/szte/skyScope/RunCucumberTest.java
@@ -5,13 +5,12 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("com.szte.skyScope")
-@org.junit.platform.suite.api.SelectDirectories("src/test/resources/skyScope")
+@SelectClasspathResource("skyScope")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.szte.skyScope.stepDefinitions")
 public class RunCucumberTest {}
